d53ffd703a1c22b5dc64c8146249f34f22c5d299
Tomek <zagorskitomasz@gmail.com>
2018-01-28 19:48:48 +0100

    created round-based game interface and move result enum

---
+++ src/main/java/com/webcheckers/api/domain/enums/MoveResult.java
@@ -0,0 +1,10 @@
+package com.webcheckers.api.domain.enums;
+
+public enum MoveResult {
+
+	MOVE_INITIALIZED,
+	MOVE_IN_PROGRESS,
+	MOVE_COMPLETED,
+	MOVE_REJECTED,
+	GAME_OVER;
+}
---
+++ src/main/java/com/webcheckers/api/domain/game/Game.java
@@ -0,0 +1,90 @@
+package com.webcheckers.api.domain.game;
+
+import java.util.List;
+
+import com.webcheckers.api.domain.enums.MoveResult;
+import com.webcheckers.api.domain.moves.Position;
+
+/**
+ * Interface of turn-based game.
+ * 
+ * @author tomek
+ *
+ */
+public interface Game {
+	
+	/**
+	 * Creating game with one pending player.
+	 * 
+	 * @param game creator
+	 * @return creating successful
+	 */
+	public boolean create(Player player);
+	
+	/**
+	 * Adding second player to game created before.
+	 * 
+	 * @param player
+	 * @return joining successful
+	 */
+	public boolean join(Player player);
+	
+	/**
+	 * Returns player that is allowed to make move.
+	 * 
+	 * @return
+	 */
+	public Player whoseMove();
+	
+	/**
+	 * Starting created game.
+	 * 
+	 * @return player that can do first move
+	 */
+	public Player start();
+	
+	/**
+	 * Asking game which player has won.
+	 * 
+	 * @return
+	 */
+	public Player whoWon();
+	
+	/**
+	 * Try make move to given localization.
+	 * 
+	 * @param position
+	 * @return
+	 */
+	public MoveResult move(Position position);
+	
+	/**
+	 * After move some items could be removed from game board.
+	 * 
+	 * @return list of items to be removed
+	 */
+	default public List<?> removeFromBoard(){
+		
+		return null;
+	}
+	
+	/**
+	 * After move some items could be added to game board.
+	 * 
+	 * @return list of items to be added
+	 */
+	default public List<?> addToBoard(){
+		
+		return null;
+	}
+	
+	/**
+	 * Sometimes games have some info for players etc.
+	 * 
+	 * @return Game message or null if nothing interesting happened.
+	 */
+	default public String getMessage() {
+		
+		return null;
+	}
+}
