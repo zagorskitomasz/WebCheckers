a86421de152e85c7efa2707d60870d0189260c2e
Adrian Sieber <mail@adriansieber.com>
2018-02-22 12:27:41 +0000

    Fix code style

--- src/main/java/com/webcheckers/api/domain/game/Board.java
+++ src/main/java/com/webcheckers/api/domain/game/Board.java
@@ -14,27 +14,27 @@ public class Board {
 	private final int HEIGHT = 10;
 	private List<Checker> allCheckers;
 	private Random generator;
-	
+
 	public Board() {
 		generator = new Random();
 		board = new Field[WIDTH][HEIGHT];
 		initializeBoard();
 	}
-	
+
 	private void initializeBoard() {
 		createFields();
 		fillWithCheckers();
 	}
 
 	private void createFields() {
-		
+
 		for(int i = 0; i < WIDTH; i++)
 			for(int j = 0; j < HEIGHT; j++)
 				board[i][j] = new Field(i, j);
 	}
-	
+
 	private void fillWithCheckers() {
-		
+
 		for(int i = 0; i < WIDTH; i++) {
 			for(int j = 0; j < HEIGHT; j++) {
 				decideIfChecker(i, j);
@@ -62,29 +62,29 @@ public class Board {
 	private void insertWhiteChecker(Field field) {
 		insertChecker(field, Color.WHITE);
 	}
-	
+
 	private void insertChecker(Field field, Color color) {
 		Checker checker = new Checker(color, field);
 		field.insertChecker(checker);
 	}
-	
+
 	public Field getField(Position position) {
 		return board[position.X][position.Y];
 	}
-	
+
 	public boolean hasChecker(Position position) {
 		return board[position.X][position.Y].hasChecker();
 	}
-	
+
 	public Checker getChecker(Position position) {
 		return board[position.X][position.Y].getChecker();
 	}
 
 	public List<Checker> getAllCheckers() {
-		
+
 		List<Checker> allCheckers = new LinkedList<>();
-		
-		for(int i = 0; i < WIDTH; i ++) {
+
+		for(int i = 0; i < WIDTH; i++) {
 			for(int j = 0; j < HEIGHT; j++) {
 				Field field = board[i][j];
 				if(field.hasChecker())
@@ -95,50 +95,50 @@ public class Board {
 	}
 
 	public List<Checker> getAllPlayerCheckers(Color color) {
-		
+
 		List<Checker> colorCheckers = getAllCheckers();
-		
-		colorCheckers.removeIf(checker -> checker.COLOR != color);
-		
+
+		colorCheckers.removeIf(checker->checker.COLOR != color);
+
 		return colorCheckers;
 	}
-	
+
 	public Checker getRandomChecker() {
-		
+
 		if(allCheckers == null) {
 			allCheckers = getAllPlayerCheckers(Color.BLACK);
 			allCheckers.addAll(getAllPlayerCheckers(Color.WHITE));
 		}
 		return allCheckers.get(generator.nextInt(allCheckers.size()));
 	}
-	
+
 	public Field getRandomEmptyField() {
-		
+
 		Field field;
 		do {
 			field = board[generator.nextInt(WIDTH)][generator.nextInt(HEIGHT)];
 		}
 		while((field.POSITION.X + field.POSITION.Y) % 2 != 0 || field.hasChecker());
-		
+
 		return field;
 	}
 
 	public void killCheckers(List<Checker> killed) {
-		
-		for(int i = 0; i < WIDTH; i ++) {
+
+		for(int i = 0; i < WIDTH; i++) {
 			for(int j = 0; j < HEIGHT; j++) {
-				
+
 				Checker checker = getChecker(new Position(i, j));
-				
+
 				if(killed.contains(checker))
 					checker.getField().removeChecker();
 			}
 		}
 	}
-	
+
 	@Override
 	public String toString() {
-		
+
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("  0 1 2 3 4 5 6 7 8 9\n");
 		for(int i = 0; i < 10; i++) {
